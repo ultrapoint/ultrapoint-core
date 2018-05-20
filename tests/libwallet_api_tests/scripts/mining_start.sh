a4c2e16ff91a4ac2bdc72918dd8488f5d750bf83
one4a11 <one4a11@msgsafe.io>
2018-05-20 06:10:09 +0100

    adjust testnet vendor default port

--- tests/libwallet_api_tests/scripts/mining_start.sh
+++ tests/libwallet_api_tests/scripts/mining_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rlwrap ultrapoint-wallet-cli --wallet-file wallet_m --password "" --testnet --trusted-daemon --daemon-address localhost:38081  --log-file wallet_m.log start_mining
+rlwrap ultrapoint-wallet-cli --wallet-file wallet_m --password "" --testnet --trusted-daemon --daemon-address localhost:27090  --log-file wallet_m.log start_mining
 
--- tests/libwallet_api_tests/scripts/mining_stop.sh
+++ tests/libwallet_api_tests/scripts/mining_stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rlwrap ultrapoint-wallet-cli --wallet-file wallet_m --password "" --testnet --trusted-daemon --daemon-address localhost:38081  --log-file wallet_miner.log stop_mining
+rlwrap ultrapoint-wallet-cli --wallet-file wallet_m --password "" --testnet --trusted-daemon --daemon-address localhost:27090  --log-file wallet_miner.log stop_mining
 
